76b7655c55651f2163c664adf66bee30826d2de5
Djole <0xdjole@gmail.com>
2025-12-03 04:37:05 +0100

    t

--- src/types/api.ts
+++ src/types/api.ts
@@ -588,6 +588,7 @@ export interface GetReservationParams {
 }
 
 export interface SearchReservationsParams {
+  query?: string;
   serviceIds?: string[];
   providerIds?: string[];
   userId?: string;
